59c3c787e41ef83c38a115153a0f6ec3f49570a9
LoganBurfield <cthulhumoose17@gmail.com>
2024-10-04 19:54:39 -0500

    Created AutonomousOpMode class and began working on it

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+//extends standard setup op mode
+public class AutonomousOpMode {
+
+    public enum COLOR {
+        RED,
+        BLUE
+    }
+
+    public enum START_POS {
+        FAR,
+        NEAR
+    }
+
+   protected MecanumDrive drive;
+
+   //@Override
+   public void runOpMode() throws InterruptedException {
+       //Parent opmode call
+       //super.runOpMode();
+   }
+
+   protected void setup(HardwareMap hardwareMap, START_POS startPos, COLOR color) {
+       //variable setup
+       }
+
+}
