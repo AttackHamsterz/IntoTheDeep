1e02865d5d3eec0805389344e126de028f2891ee
MQQN-LTGHT <112734547+MQQN-LTGHT@users.noreply.github.com>
2024-10-06 18:37:03 -0500

    started autonomous blue right blocking!! yay!! go team

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueRight.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
-public class AutonomousBlueRight {
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.tuning.TuningOpModes;
+
+public class AutonomousBlueRight extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException{
+        Pose2d beginPose = new Pose2d (0, 0, 0);
+        if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
+            MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+            waitForStart();
+
+            Actions.runBlocking(
+                    drive.actionBuilder(beginPose)
+                            .splineTo(new Vector2d(20,0),0)
+                            .waitSeconds(2.0)
+                            .turnTo(Math.toRadians(90.0))
+                            .build()
+            );
+
+        }
+    }
+
 }
