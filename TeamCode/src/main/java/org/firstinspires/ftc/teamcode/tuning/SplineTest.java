8178eeaaf2d6736d9de51d3953d40e30f1f74ca1
Mizukiee <codingforfun156@gmail.com>
2024-10-06 19:59:47 -0500

    Blocked out blue right auton and adjusted some values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
@@ -18,12 +18,26 @@ public final class SplineTest extends LinearOpMode {
             waitForStart();
 
             Actions.runBlocking(
-                drive.actionBuilder(beginPose)
-                        .splineTo(new Vector2d(20, 0), 0)
-                        .turnTo(Math.PI/2)
-                        .waitSeconds(2.0)
-                        .strafeTo(new Vector2d( 0, 0))
-                        .build());
+                    drive.actionBuilder(beginPose)
+                            // move forward towards the submersible
+                            .splineTo(new Vector2d(7,0),0)
+                            // raise arm to place specimen
+                            // place specimen on the bar
+                            .waitSeconds(2.0)
+                            // turn towards yellow samples
+                            //move towards samples
+                            .strafeToLinearHeading(new Vector2d(7+11*Math.sqrt(2) , -11*Math.sqrt(2)), -(Math.PI / 4))
+                            // extend the arm to grab one sample
+                            .waitSeconds(2.0)
+                            //turn to face basket
+                            .turn(Math.PI)
+                            .waitSeconds(2.0)
+                            // move towards basket
+                            //.lineToY(40)
+                            // raise arm
+                            // place sample in basket
+                            .build()
+            );
         } else if (TuningOpModes.DRIVE_CLASS.equals(TankDrive.class)) {
             TankDrive drive = new TankDrive(hardwareMap, beginPose);
 
