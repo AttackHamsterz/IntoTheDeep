0e734f569302dd44481e1e7ea2ffe4177be209eb
MQQN-LTGHT <112734547+MQQN-LTGHT@users.noreply.github.com>
2024-10-08 19:09:36 -0500

    finished most of standard setup op mode!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StandardSetupOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StandardSetupOpMode.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name = "Robot Setup Super Class", group = "Robot")
@@ -36,6 +38,29 @@ public class StandardSetupOpMode {
         armMotorLeft = hardwareMap.get(DcMotor.class, "armMotorLeft"); //ch1 expansion hub Motor
         armMotorRight = hardwareMap.get(DcMotor.class, "armMotorRight"); //ch2 expansion hub Motor
         shoulderMotor = hardwareMap.get(DcMotor.class, "shoulderMotor"); //ch0 Motor
+
+        //setting the motor directions
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        rearLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rearRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        armMotorLeft.setDirection(DcMotor.Direction.FORWARD);
+        armMotorRight.setDirection(DcMotor.Direction.REVERSE);
+        shoulderMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        //setting type of run mode --> different ways to have ur motor run
+        armMotorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shoulderMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        shoulderMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //Build the classes and give in an object
+        motion = new Motion(frontLeftDrive, frontRightDrive, rearLeftDrive, rearRightDrive, null);
+        arm = new Arm(armMotorLeft, armMotorRight, null);
+        shoulder = new Shoulder(shoulderMotor, gamepad2);
+
 
     }
 
