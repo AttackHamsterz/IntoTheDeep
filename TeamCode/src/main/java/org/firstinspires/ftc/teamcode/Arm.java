2427864148693e3591420e38e4ba965a346dacf6
MQQN-LTGHT <112734547+MQQN-LTGHT@users.noreply.github.com>
2024-10-13 19:08:05 -0500

    Finished copying arm and shoulder code from centerstage and fixed errors in them.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.Range;
 
 public class Arm extends Thread{
     //min and max speed of the arm  🦾🚗💨
@@ -11,13 +12,14 @@ public class Arm extends Thread{
     //min and max rotation/position(pos) of the arm 🔄
     private static final int MIN_POS = 0;
     private static final int MAX_POS = 1000;
+    private static int SHOULDER_MAX;
 
     //variables (vars) for the arm motors  🦾🐝
     private final DcMotor armMotorLeft;
     private final DcMotor armMotorRight;
 
     //vars for the shoulder and gamepad for the constructor 🎮🛠️
-    //private Shoulder shoulder;
+    private Shoulder shoulder;
     private final Gamepad gamepad;
 
     //vars for keeping track of arm position  🦾📍
@@ -25,7 +27,7 @@ public class Arm extends Thread{
     private int totalCountsRight;
 
     /**
-     * Constroctor for the arm!!! 🦾🛠️
+     * Constructor for the arm!!! 🦾🛠️
      * @param armMotorLeft
      * @param armMotorRight
      * @param gamepad
@@ -37,4 +39,51 @@ public class Arm extends Thread{
         this.armMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         this.armMotorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
+    public void setShoulder(Shoulder shoulder) {
+        this.shoulder = shoulder;
+    }
+
+    protected int getArmCounts1() {
+        return totalCountsLeft;
+    }
+
+    protected int getArmCounts2() {
+        return totalCountsRight;
+    }
+
+    /**
+     * Gets the ratio from 0 to 1 inclusive of how far the arm is extended
+     * @return [0, 1]
+     */
+    public double getArmRatio() {return Range.clip((double) totalCountsLeft / (double) MAX_POS, 0.0, 1.0);}
+
+    private void setPosition(double power, int position) {
+        power = Range.clip(power, MIN_ARM_SPEED, MAX_ARM_SPEED);
+        armMotorLeft.setPower(power);
+        armMotorRight.setPower(power);
+        armMotorRight.setTargetPosition(position);
+        armMotorLeft.setTargetPosition(position);
+    }
+
+    @Override
+    public void run() {
+        while (!isInterrupted()) {
+
+            if (shoulder != null) {
+                SHOULDER_MAX = (int) Math.round(shoulder.getShoulderRatio());
+            } else {
+                SHOULDER_MAX = 0;
+            }
+
+            totalCountsLeft = armMotorLeft.getCurrentPosition();
+            totalCountsRight = armMotorRight.getCurrentPosition();
+
+            double ARM_SPEED = gamepad.left_stick_y;
+            int pos = (ARM_SPEED >= 0) ? MIN_POS:MAX_POS;
+            setPosition(ARM_SPEED, pos);
+
+        }
+    }
+
+
 }
