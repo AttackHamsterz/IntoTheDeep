df5a3c45238dbffe51638ce3d5d29f3aecdd9069
Makayla McDonnell <makaylamcdonnell@gmail.com>
2024-11-05 18:49:27 -0600

    Tweaks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueRight.java
@@ -98,12 +98,37 @@ public class AutonomousBlueRight extends LinearOpMode {
             // This action will determine where we are (might check apiril tags as well)
             // Then move to grab another floor sample, finally stowing for travel.
             ParallelAction floorSample = new ParallelAction(
-
+                    drive.actionBuilder(drive.pose)
+                        //movement code
+                        //moving to the three lines with the samples
+                        //may just be turning with no movement
+                        .build(),
+                    new Action() {
+                        @Override
+                        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                            //arms.setPosition(out);
+                            //fingers.search();
+                            //fingers.pickup();
+                            //arms.setPosition(stow);
+                            return false;
+                        }
+                    }
             );
 
             // Determine where we are and how we have to move to park correctly
             ParallelAction park = new ParallelAction(
-
+                    drive.actionBuilder(drive.pose)
+                            //movement code
+                            //moving to the three lines with the samples
+                            //may just be turning with no movement
+                            .build(),
+                    new Action() {
+                        @Override
+                        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                            //shoulder.setPosition(hang);
+                            return false;
+                        }
+                    }
             );
 
             waitForStart();
